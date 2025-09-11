19dd9b58169ab20438421858a4f55a239fb28213
AbnormalMatrix <babin.carter@gmail.com>
2025-09-11 09:27:39 -0600

    Add the week 2 page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    const studentName = "Carter";
+    const githubLink = "https://github.com/AbnormalMatrix"
+    return (
+        <div>
+            <p>{studentName}</p>
+            <Link href={githubLink}>{githubLink}</Link>
+        </div>
+        
+
+    )
+}
