1b1737772df914b6b9619d579014df8450dafe8f
Maxwell9-9 <Chukwudumebi.Anaruagu@edu.sait.ca>
2025-09-28 20:46:38 -0600

    minor change

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -19,8 +19,8 @@ export default function NewItem() {
 
     return (
         <div className="flex  justify-center min-h-screen">
-            <p className="flex items-center justify-center border border-gray-500 rounded w-min h-min p-3">
-                <span className="text-black w-12 text-lg font-extrabold text-center border border-black rounded">{getQuantity}</span>
+            <p className="flex bg-gray-100 items-center justify-center border border-gray-500 rounded w-min h-min p-3">
+                <span className="text-gray-500 w-12 text-lg font-extrabold text-center border border-gray-500 rounded">{getQuantity}</span>
                 {getQuantity > 1 ? <button className="bg-blue-600 text-white p-3 text-xl mx-2 rounded hover:bg-blue-700"onClick={decrement}>-</button> : <button className="bg-gray-600 text-white p-3 text-xl mx-2 rounded hover:cursor-not-allowed" disabled>-</button>}
                 {getQuantity < 20 ? <button className="bg-blue-600 text-white p-3 text-xl mx-2 rounded hover:bg-blue-700" onClick={increment}>+</button> : <button className="bg-gray-600 text-white p-3 text-xl mx-2 rounded hover:cursor-not-allowed"disabled>+</button>}
             </p>
