444dccd0a8eafcf451d6f5382372910c8ee26e4c
reytacarreon <reyta.carreon@edu.sait.ca>
2025-10-04 00:27:45 -0600

    Update page.js

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -2,10 +2,6 @@ import NewItem from "./new-item";
 
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-2xl">Week 4</h1>
-        <NewItem />
-
     <div className="h-screen bg-black flex items-center justify-center">
       
       <NewItem />
