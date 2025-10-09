74a1f11289e8d5cc5bbff1781cdb2e56a5ab9a92
AbnormalMatrix <babin.carter@gmail.com>
2025-10-09 08:42:51 -0600

    Add the week 6 files

---
+++ app/week-6/item-list.js
@@ -0,0 +1,51 @@
+"use client"
+
+import { useState } from "react";
+import Item from "./item";
+import data from "./item.json";
+export default function ItemList() {
+    const [sortBy, setSortBy] = useState("name");
+
+    const compareByName = function(a, b) {
+            if (a.name > b.name) {
+                return 1;
+            } else if (a.name < b.name) {
+                return -1;
+            } else {
+                return 0;
+            }
+    }
+    const compareByCategory = function(a, b) {
+            if (a.category > b.category) {
+                return 1;
+            } else if (a.category < b.category) {
+                return -1;
+            } else {
+                return 0;
+            }
+    } 
+
+    const sortItems = function() {
+        console.log("sorting");
+        switch (sortBy) {
+            case "name": setSortBy(data.sort(compareByName)); break;
+            case "category": setSortBy(data.sort(compareByCategory)); break;
+        }
+    }
+
+    
+
+    return (
+        <div>
+            <button onClick={() => { setSortBy("name"); sortItems(); }}>Sort by name</button>
+            <button onClick={() => { setSortBy("category"); sortItems(); }}>Sort by category</button>
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>Name: {item.name}, Category: {item.category}</li>
+                ))}
+            </ul>
+        </div>
+
+    )
+}
+
---
+++ app/week-6/page.js
@@ -0,0 +1,13 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-2xl">Week 3</h1>
+      <h1 className="text-2xl">Shopping List</h1>
+      <ul>
+        <ItemList />
+      </ul>
+    </div>
+  );
+}
