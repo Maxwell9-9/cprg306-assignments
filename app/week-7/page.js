1e9971ed403f43e46e984ac31941ced6fa5468f9
reytacarreon <reyta.carreon@edu.sait.ca>
2025-10-22 03:34:27 -0600

    Update page.js

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -15,7 +15,7 @@ export default function Page(){
 
 
   return (
-    <main className="p-6">
+    <main className="max-w-2xl mx-auto px-4 py-6">
       <h1 className="text-3xl font-bold mb-4">Week 7 - Shopping List </h1>
       <NewItem onAddItem={handleAddItem} />
       <ItemList items={items} />
